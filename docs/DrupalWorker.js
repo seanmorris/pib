47e4b5f9fe882a0d262e0fa6504ff4d716829d67
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-08 22:50:07 -0400

    Tweaks.

--- docs/DrupalWorker.js
+++ docs/DrupalWorker.js
@@ -216,6 +216,7 @@ self.addEventListener('message', function (event) {});
 self.addEventListener('push', function (event) {
   console.log(event);
 });
+
 });
 
 require.register("___globals___", function(exports, require, module) {
@@ -223,4 +224,3 @@ require.register("___globals___", function(exports, require, module) {
 });})();require('___globals___');
 
 require('DrupalWorker');
-//# sourceMappingURL=DrupalWorker.js.map
