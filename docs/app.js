465b8640709d937bc88c8219b962b54f7f69f0c0
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-26 08:54:27 -0400

    JS integration

--- docs/app.js
+++ docs/app.js
@@ -13789,11 +13789,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
 });
 
 require.register("___globals___", function(exports, require, module) {
   
 });})();require('___globals___');
 
-
-//# sourceMappingURL=app.js.map
